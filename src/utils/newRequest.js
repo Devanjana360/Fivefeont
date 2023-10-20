a89220681914b90ea53521fc074235e519d31d79
Anjana Induwara <36652213+Devanjana360@users.noreply.github.com>
2023-10-20 20:31:16 +0530

    Update newRequest.js

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://fiveback-rqwx6.kinsta.app/api/",
   withCredentials: true,
 });
 
